744251348205c33ba62a3774c5ee7c785bb569a0
ivan-nr <ivanngudi16@gmail.com>
2024-09-21 13:48:53 +0700

    feat: add auth api route

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import cors from "cors";
+import authRoute from "./routes/authRoutes";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoute);
 
 export default app;
